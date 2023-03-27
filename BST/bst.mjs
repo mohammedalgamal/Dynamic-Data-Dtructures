6fe0574b06ac8eaf20b163b819f223458f40c608
mohammedalgamal <mohammedalgamal95@gmail.com>
2023-03-27 14:21:51 +0200

    Add prettyPrint method to Tree class

--- BST/bst.mjs
+++ BST/bst.mjs
@@ -9,4 +9,22 @@ class Tree {
     buildTree(array) {
         return;
     }
+
+    prettyPrint(node, prefix = '', isLeft = true) {
+
+        if (node === null) {
+           return;
+        };
+
+        if (node.right !== null) {
+          prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+        };
+
+        console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+
+        if (node.left !== null) {
+          prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+        };
+        
+      }
 }
