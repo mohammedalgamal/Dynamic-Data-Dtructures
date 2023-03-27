dc77f55f21f0347d40ce045dcfce973e5cb220f1
mohammedalgamal <mohammedalgamal95@gmail.com>
2023-03-27 13:18:45 +0200

    Make linkedList class

--- Linked list/linkedList.js
+++ Linked list/linkedList.js
@@ -1,3 +1,10 @@
+class LinkedList {
+    constructor () {
+        this.head = null;
+        this.length = 0;
+    };
+}
+
 class Node {
     constructor(value = null, nextNode = null) {
         this.value = value;
