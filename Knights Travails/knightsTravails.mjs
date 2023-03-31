dff3446c50a4ee1762dfd10c6670516767acf9e0
mohammedalgamal <mohammedalgamal95@gmail.com>
2023-03-31 21:13:25 +0200

    Add printGraph method to Graph class

--- Knights Travails/knightsTravails.mjs
+++ Knights Travails/knightsTravails.mjs
@@ -2,23 +2,52 @@ import Knight from "./knight.mjs";
 
 class MovesGraph {
     constructor() {
-        this.graph = this.makeAllPossibleMoves();
+        this.adjList = this.makeAllPossibleMoves();
     };
 
     makeAllPossibleMoves() {
-        const graph = [];
+        const graph = new Map();
 
         for (let i = 0; i < 8; i++) {
             for (let j = 0; j < 8; j++) {
                 const newKnight = new Knight([i, j]);
-                graph.push(newKnight);
+                graph.set(newKnight.currentPosition, newKnight.nextMoves);
             };
         };
 
         return graph;
     };
+
+    searchGraph(start, target) {
+
+    };
+
+    knightMoves(currentPosition, targetPosition) {
+        const path = this.searchGraph(currentPosition, targetPosition);
+        const outputStr = `You made it in ${path.length} moves!  Here's your path:`;
+
+        for (let i = 0; i < path.length; i++) {
+            outputStr += `${path[i]}\n`;
+        };
+
+        console.log(outputStr);
+    };
+
+    printGraph() {
+        let keys = this.adjList.keys();
+    
+        for (let i of keys) {
+            let values = this.adjList.get(i);
+            let conc = "";
+
+            for (let j of values) {
+                conc += `[${j}] `;
+            };
+            
+            console.log(`[${i}] -> ${conc}`);
+        };
+    };
 };
 
 const graph = new MovesGraph;
-
-console.log(graph.graph);
+graph.printGraph();
